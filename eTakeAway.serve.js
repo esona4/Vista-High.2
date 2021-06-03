2806718bcb49cd8aa543887cfb7dfcb488a00df9
esona4 <84981022+esona4@users.noreply.github.com>
2021-06-03 12:33:24 +0200

    Delete eTakeAway.serve.js

--- eTakeAway.serve.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("button").onclick = function(){
-    document.getElementById("confirming").innerHTML = "OK, your order is 100% sorted. Please check email if you want to be sure.";
-    document.getElementById("button").style.display = "none";
-}  
