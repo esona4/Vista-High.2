b2e15fe5a4d41dd93b500357189d39c15333bb77
esona4 <84981022+esona4@users.noreply.github.com>
2021-06-03 12:31:07 +0200

    Add files via upload

---
+++ eTakeAway.serve.js
@@ -0,0 +1,4 @@
+document.getElementById("button").onclick = function(){
+    document.getElementById("confirming").innerHTML = "OK, your order is 100% sorted. Please check email if you want to be sure.";
+    document.getElementById("button").style.display = "none";
+}  
